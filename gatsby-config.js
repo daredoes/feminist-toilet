652d333e4c17e594f2f9dec621a9c623592a895b
Daniel Evans <migetman9@gmail.com>
2019-01-13 13:45:40 -0800

    update site description

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,7 +1,7 @@
 module.exports = {
 	siteMetadata: {
 		title: 'Feminist Toilet',
-		description: "Toilets are the ultimate feminists. Tell me one person you know that doesn't shit. Exactly. Everyone has experiences with toilets. They are liminal spaces where society meets humanity - where we let our animal side out. I mean it's a bit weird what happens in those sanitary sanctuaries, and yet never talk about it. It's fucking earthly in there. I think what happens in toilets is beautiful. It is the place one can be most vulnerable. It is a safe space, a private place in a public world.\n Please enjoy these poems, prose pieces, essays, images, and artwork inspired by the toilet, our beloved porcelain deity. ",
+		description: "How vulnerable can you be half naked on the toilet with your pants around your ankles and just a little bit of shit hanging from your asshole. Pretty fucking vulnerable, I will tell you that.",
 	},
 	mapping: {
 		'MarkdownRemark.frontmatter.author': `MarkdownRemark.frontmatter.name`,
